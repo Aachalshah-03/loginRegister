a895bd2df8479df2d5d3192f0a1c88d57528b106
Aachal Shah <aachal0304shah@gmail.com>
2026-04-28 17:23:13 +0530

    remove broken backend

--- backend/frontend/src/pages/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-function Dashboard() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>Welcome to Dashboard 🎉</h1>
-    </div>
-  );
-}
-
-export default Dashboard;
